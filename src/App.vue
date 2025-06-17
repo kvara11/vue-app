bcbafaf4b98aef9d016ecfac9c345da64d03796a
Nikoloz Kvaratskehlia <nikakvaratskhelia11@gmail.com>
2025-06-17 16:40:48 +0400

    initial publish

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { authService } from './services/authService'
+import Login from './components/Login.vue'
+// import MainLayout from './components/MainLayout.vue'
+
+const { user } = authService()
+const isLoggedIn = computed(() => user.value !== null)
+</script>
+
+<template>
+  <div>
+    <Login v-if="!isLoggedIn" />
+    <!-- <MainLayout v-else /> -->
+  </div>
+</template>
---
+++ src/services/authService.ts
@@ -0,0 +1,38 @@
+// src/composables/useAuth.js
+import { ref } from 'vue'
+
+const user = ref(JSON.parse(localStorage.getItem('user') || 'null'))
+
+const testUser = {
+  username: 'admin',
+  password: 'admin',
+};
+
+
+function login(username :string, password: string) {
+
+  if (username === testUser.username && password === testUser.password) {
+    user.value = { username }
+    localStorage.setItem('user', JSON.stringify(user.value))
+    return true
+  }
+  return false
+}
+
+function logout() {
+  user.value = null
+  localStorage.removeItem('user')
+}
+
+function isLoggedIn() {
+  return !!user.value
+}
+
+export function authService() {
+  return {
+    user,
+    login,
+    logout,
+    isLoggedIn,
+  }
+}
